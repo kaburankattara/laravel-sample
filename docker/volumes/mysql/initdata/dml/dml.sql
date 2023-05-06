863f344f1fbbaca0e70cef4514e636f08bb4bb4a
kaburankattara <kaburankattara@gmail.com>
2023-05-06 22:55:22 +0900

    MySQLコンテナ起動時、データを投入するように実装

---
+++ docker/volumes/mysql/initdata/dml/dml.sql
@@ -0,0 +1,2 @@
+load data infile '/initdata/dml/csv/sample.csv' into table `sample` fields terminated by ',' enclosed by '"';
+load data infile '/initdata/dml/csv/sample_element.csv' into table sample_element fields terminated by ',' enclosed by '"';
--- docker/volumes/mysql/initdb.d/3_execute_ddl_dml_to_develop.sh
+++ docker/volumes/mysql/initdb.d/3_execute_ddl_dml_to_develop.sh
@@ -3,3 +3,7 @@ set -e
 
 # DDLの実行
 mysql -u ${DEVELOP_USER_NAME} -p${DEVELOP_USER_PASSWORD} ${DEVELOP_DATABASE_NAME} < /initdata/ddl/ddl.sql
+
+# DMLの実行
+mysql -u root -p${MYSQL_ROOT_PASSWORD} ${DEVELOP_DATABASE_NAME} < /initdata/dml/dml.sql
+
